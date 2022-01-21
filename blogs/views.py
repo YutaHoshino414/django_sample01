a18da15f93cf0a83b4cf77be9867ecbb7070d0b2
USER <y.hoshino25@gmail.com>
2022-01-21 22:50:39 +0900

    add POST_index/detail views

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post, Category
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+def Index(request):
+    #posts = Post.objects.all().order_by('id')
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    posts = Post.objects.filter(category__name__icontains=q)
+
+    categories = Category.objects.all()
+
+    context = {'posts': posts, 'categories':categories}
+    return render(request, 'blogs/post_list.html', context)
+
+def Detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'blogs/post_detail.html', {'post': post})
