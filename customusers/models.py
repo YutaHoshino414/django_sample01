4b1dc950164a088d22bd356ea1957d5bd83cf943
USER <y.hoshino25@gmail.com>
2022-02-05 23:04:42 +0900

    add USER_Model

---
+++ customusers/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.core.mail import send_mail
+from django.utils import timezone
+from django.contrib.auth.base_user import BaseUserManager
+
+
+# Create your models here.
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+# email を必須にする
+        if not email:
+            raise ValueError('Emailを入力して下さい')
+# email で User モデルを作成
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('is_staff=Trueである必要があります。')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('is_superuser=Trueである必要があります。')
+        return self._create_user(email, password, **extra_fields)
+
+# カスタムユーザーモデル
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField("メールアドレス", unique=True)
+    is_staff = models.BooleanField("is_staff", default=False)
+    is_active = models.BooleanField("is_active", default=True)
+    date_joined = models.DateTimeField("date_joined", default=timezone.now)
+
+    objects = UserManager()
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "customusers"
